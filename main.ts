14e5257b273672ee1d014dd37ef8eb704af506bc
lalaluplalalup-dotcom <lalaluplalalup@gmail.com>
2025-12-17 10:13:47 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
-input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
-    basic.clearScreen()
-})
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     record.startRecording(record.BlockingState.Blocking)
+    control2 = 0
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     record.playAudio(record.BlockingState.Blocking)
 })
-input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
-    basic.showNumber(input.lightLevel())
-})
+let control2 = 0
+music.setVolume(255)
 record.setMicGain(record.AudioLevels.Low)
+control2 = 1
+basic.forever(function () {
+    if (input.lightLevel() > 10) {
+        if (control2 == 0) {
+            record.playAudio(record.BlockingState.Blocking)
+            control2 = 1
+        }
+    }
+})
